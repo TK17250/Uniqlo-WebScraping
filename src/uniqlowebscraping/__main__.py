db39eb91843b1076fc21f6c302224fb9c420914e
TK17250 <TXK285@gmail.com>
2024-04-20 15:09:48 +0700

    Change library don't use selenium and scraping convert to csv.

--- src/uniqlowebscraping/__main__.py
+++ src/uniqlowebscraping/__main__.py
@@ -1,63 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import requests
+import json
 import pandas as pd
 
 # Set up
-df = pd.DataFrame(columns=["Name", "Price", "page"]) # Create a dataframe
-driver = webdriver.Chrome() # Using bowser
-link_to_item = [] # List of links to items
+headers = {
+  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36',
+}
+amount_of_item = 300
 
-num_items = 5 # Number of items
-driver.get("https://www.uniqlo.com/th/en/men/tops/tops-collections") # Go to the website
+# URL
+url = f"https://www.uniqlo.com/th/api/commerce/v3/en/products?path=%2C%2C25997&limit={amount_of_item}"
 
-print("\n==================== Start scraping ====================\n")
-items = driver.find_elements(By.XPATH, "//article[@class='fr-grid-item w4']") # Find all items
+# Process
+res = requests.get(url, headers=headers)
+data_dumps = json.dumps(res.json()) # Convert to string
+data = json.loads(data_dumps) # Convert to json
 
-
-i = 0 # Counter
-# while True:
-#     items = driver.find_elements(By.XPATH, "//article[@class='fr-grid-item w4']") # Find all items
-#     load_more = driver.find_element(By.CSS_SELECTOR, "div.fr-load-more")
-#     load_more.click()
-#     print(items)
-
-#     if not load_more:
-#         break
-
-for item in items:
-    item_page = item.find_element(By.TAG_NAME, "a").get_attribute("href")
-    link_to_item.append(item_page)
-    i += 1
-    driver.implicitly_wait(10)
-
-    if i >= num_items:
-        driver.quit()
-        break
-
-    # name = item.find_element(By.TAG_NAME, "h2").text # Find a name
-
-    # # Find a price
-    # Boxprice = item.find_elements(By.CSS_SELECTOR, "span.price-original span.fr-price-currency")
-    # for textPrice in Boxprice:
-    #     price = textPrice.find_element(By.TAG_NAME, "span").text
-
-    # Go to item page
-    # driver.get(f"https://uniqlo.com/{item_page}")
-
-    # https://uniqlo.com/https://www.uniqlo.com/th/en/products/E464857-000
-
-    # item_in_page = driver.find_element(By.TAG_NAME, "body")
-
-    # name = item_in_page.find_element(By.CSS_SELECTOR, "span.title span.fr-no-uppercase").text # Find a name
-    
-    # # Find a price
-    # Boxprice = item_in_page.find_elements(By.CSS_SELECTOR, "span.fr-price-currency span.xN1x05O0jJUkQaFHARtFH")
-    # price = Boxprice.find_element(By.TAG_NAME, "span").text
-
-    # data_test = item.get_attribute("data-test")
-    # new_row = {"Name": name, "Price": price, "page": item_page}
-    # df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-
-    # driver.back()
-
-print(link_to_item)
+# Show the data
+df = pd.DataFrame(data["result"]["items"])
+df.to_csv("uniqlo.csv", index=False)
